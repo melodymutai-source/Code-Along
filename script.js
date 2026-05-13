ae10cf09f31df80e21dceea4950f9b188886f4bc
melody mutai <chepngenomutai99@gmail.com>
2026-05-13 12:56:06 +0300

    rename contact constructor to Contact to fix reference errors

--- script.js
+++ script.js
@@ -1,46 +1,46 @@
-function contact(firstName,lastName,phoneNumber) {
+function Contact(firstName,lastName,phoneNumber) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.phoneNumber = phoneNumber;
 }
 function AddressBook(){
-    this.contacts ={};
+    this.Contacts ={};
     this.currentId = 0;
 }
 
 AddressBook.prototype.addContact = function(contact) {
     this.currentId += 1;
     contact.id = this.currentId;
-    this.contacts[contact.id] = contact;
+    this.Contacts[contact.id] = contact;
 };
 
 AddressBook.prototype.findContact = function(id) {
-    if (this.contacts[id] !==undefined) {
-        return this.contacts[id];
+    if (this.Contacts[id] !==undefined) {
+        return this.Contacts[id];
     }
     return false;
 };
 
 AddressBook.prototype.deleteContact =function(id) {
-    if (this.contact[id] === undefined) {
+    if (this.Contact[id] === undefined) {
         return false;
     }
-    delete this.contacts[id];
+    delete this.Contacts[id];
     return true;
 };
 
 let addressBook = new AddressBook();
 
-window.addEventListener("load",function(){
+window.addEventListener("load",function() {
     document.querySelector("form#new-contact").addEventListener("submit",function(event) {
         event.preventDefault();
         const inputtedFirstName = document.querySelector("input#new-first-name").value;
         const inputtedLastName = document.querySelector("input#new-last-name").value;
         const inputtedphoneNumber = document.querySelector("input#new-phone-number").value;
 
-        let newContact = newContact(inputtedFirstName,inputtedLastName,inputtedphoneNumber);
+        let newContact = new Contact(inputtedFirstName,inputtedLastName,inputtedphoneNumber);
         addressBook.addContact(newContact);
 
-        console.log(addressBook.contacts);
+        console.log(addressBook.Contacts);
     });
 });
